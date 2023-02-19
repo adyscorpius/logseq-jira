5d52fa3a4231fab3b82d297d414bb8621f819bcb
Adit Sharda <mailme@aditsharda.com>
2023-02-19 04:31:39 -0500

    Fully refactor code and add support for block properties.

--- src/index.ts
+++ src/index.ts
@@ -27,21 +27,36 @@ function main() {
 }
 
 
+type Data = {
+    [issueKey: string]: {
+      text: string;
+      status: string;
+      type: string;
+      priority: string;
+      creator: string;
+      reporter: string;
+      assignee: string;
+      fixVersion: string;
+    };
+  };
+
 // Regex declarations
 // DEV-1000
-const issueKeyRegex: RegExp = /(?<![\,\.\/\S])(?<issue>[A-Z][A-Z0-9]+-[0-9]+)(?!.?\])/gim;
 
-// https://company.atlassian.net/browse/DEV-1000
-const jiraRegex: RegExp = /(?<!\()(?<url>https*:\/\/.{1,25}.atlassian.net\/browse\/(?<issue>[A-Z][A-Z0-9]{1,6}-[0-9]{1,8}))(?!\))/gim;
+const regexList = [
 
-// [Some Jira status text we could update](https://company.atlassian.net/browse/DEV-1000)
-const jiraLinkRegex: RegExp = /\[(?<description>[^\]]*)?\]\((?<url>https?:\/\/[A-Za-z0-9 ]+\.atlassian\.net\/browse\/(?<issue>[A-Za-z0-9\-]+))\)/gim;
+    // DEV-1001
+    /(?<![\,\.\/\S])(?<issue>[A-Z][A-Z0-9]+-[0-9]+)(?!.?\])/gim, 
 
-// [A Markdown URL](https://some.other-url.com/we-dont-care-about)
-const markdownLinkRegex: RegExp = /(?:__|[*#])|\[(.*?)\]\(.*?\)/gi;
+    // https://company.atlassian.net/browse/DEV-1000
+    /(?<!\()(?<url>https*:\/\/.{1,25}.atlassian.net\/browse\/(?<issue>[A-Z][A-Z0-9]{1,6}-[0-9]{1,8}))(?!\))/gim,
+
+    // [Some Jira status text we could update](https://company.atlassian.net/browse/DEV-1000)
+    /\[(?<description>[^\]]*)?\]\((?<url>https?:\/\/[A-Za-z0-9 ]+\.atlassian\.net\/browse\/(?<issue>[A-Za-z0-9\-]+))\)/gim
+];
 
 // DEV-1000 https://company.atlassian.net/browse/DEV-1000 (basically everything that has an issueKey)
-const issueTestRegex: RegExp = /([A-Z][A-Z0-9]+-[0-9]+)/i
+const issueTestRegex: RegExp = /([A-Z][A-Z0-9]+-[0-9]+)/gim
 
 
 
@@ -52,75 +67,127 @@ async function updateJiraIssue() {
         // Get current block value
         const currentBlock = await logseq.Editor.getCurrentBlock();
         let value = currentBlock?.content;
-
+        const uuid = currentBlock?.uuid;
+        
         if (!value || value.length < 3 || !issueTestRegex.test(value)) {                                               // TODO: Find a better logic?
             logseq.UI.showMsg("Couldn't find a valid Jira issue key.", 'error');
             return;
         };
 
-        value = await replaceAsync(value, 'issueKey', generateTextFromAPI);
-        value = await replaceAsync(value, 'jiraLink', generateTextFromAPI);
-        value = await replaceAsync(value, 'markdown', generateTextFromAPI);
-        await logseq.Editor.updateBlock(currentBlock.uuid, value);
+        
+        const issuesList = getIssuesList(value);
+
+        if (!issuesList || issuesList.length < 1) {
+            logseq.UI.showMsg("Couldn't find any Jira issues.", 'error');
+            return;
+        };
+
+        
+        const issues = await getIssues(issuesList);
+        
+        const data = generateTextFromResponse(issues);
+        
+        const newValue = await replaceAsync(value, data);
+
+        // const properties = {};      // Doesn't work cuz updateBlock won't also update properties in one click.  
+
+        // if (issuesList?.length === 1) {
+        //     let issueKey = issuesList[0];
+        //     console.log("One issue found.");
+        //     logseq.settings?.jiraShowAssignee ? properties["assignee"] = data[issueKey].assignee || '' : null;
+        //     logseq.settings?.jiraShowPriority ? properties["priority"] = data[issueKey].priority : null;
+        //     logseq.settings?.jiraShowFixVersion ? properties["fix-version"] = data[issueKey].fixVersion || '' : null;
+        //     logseq.settings?.jiraShowStatus ? properties["status"] = data[issueKey].status : null;
+        //     logseq.settings?.jiraShowReporter ? properties["reporter"] = data[issueKey].reporter : null;
+        //     console.log(properties);
+        // }
+
+        await logseq.Editor.updateBlock(currentBlock.uuid, newValue);
+
+        if (issuesList?.length === 1) {
+            let issueKey = issuesList[0];
+            console.log("One issue found.");
+            logseq.settings?.jiraShowAssignee && await logseq.Editor.upsertBlockProperty(uuid, "assignee", data[issueKey].assignee);
+            logseq.settings?.jiraShowPriority && await logseq.Editor.upsertBlockProperty(uuid, "priority", data[issueKey].priority);
+            logseq.settings?.jiraShowFixVersion && await logseq.Editor.upsertBlockProperty(uuid, "fix-version", data[issueKey].fixVersion);
+            logseq.settings?.jiraShowStatus && await logseq.Editor.upsertBlockProperty(uuid, "status", data[issueKey].status);
+            logseq.settings?.jiraShowReporter && await logseq.Editor.upsertBlockProperty(uuid, "reporter", data[issueKey].reporter);
+        }
+        
+        
+        //await logseq.Editor.updateBlock(currentBlock.uuid, newValue, { properties: properties });
+
         logseq.UI.showMsg('Updated all JIRA links found.')
     } catch (e) {
         console.log('logseq-jira', e.message);
     }
 }
 
-// Helper function run regex replacements asynchronously and as soon as possible.
-async function replaceAsync(
-    str: string,
-    regexType: 'issueKey' | 'jiraLink' | 'markdown',
-    getAPIResponseText: any) {
-
-    const isIssueType = regexType === 'issueKey';
-    const isJiraLink = regexType === 'jiraLink';
-    let regex: RegExp = isIssueType ? issueKeyRegex : isJiraLink ? jiraRegex : jiraLinkRegex;
-    const promises = (Array.from(str.matchAll(regex), (match) => {
-        return getAPIResponseText(match.groups.issue);
-    }));
-    const data = await Promise.all(promises);
-    return str.replace(regex, () => data.shift());
+// Get issues from block text.
+function getIssuesList(str: string): Array<string> {
+    return [...new Set(str.match(issueTestRegex))];
 }
 
+// Get issues from Jira
+async function getIssues(issuesList: Array<string>) {
 
-// Make API call to Atlassian for generating new text from from Jira Issue key
-async function generateTextFromAPI(issueKey: string): Promise<string> {
-    try {
-        if (!issueTestRegex.test(issueKey)) {
-            console.log(`logseq-jira: Badly structured issueKey ${issueKey}`);
-        }
-
-        const baseURL = logseq.settings?.jiraBaseURL;
-        if (!baseURL) {
-            logseq.UI.showMsg('Jira base URL not set. Update in Plugin settings.')
-            throw new Error('Jira base URL not set.');
-        }
-
-        const creds = Buffer.from(`${logseq.settings?.jiraUsername}:${logseq.settings?.jiraAPIToken}`).toString("base64");
-        const issueRest = `https://${baseURL}/rest/api/3/issue/${issueKey}`;
-        const jiraURL = `https://${baseURL}/browse/${issueKey}`;
-        const response = await fetch(issueRest, {
-            headers: {
-                'Accept': 'application/json',
-                'Authorization': `Basic ${creds}`
+    const promises = issuesList.map(async (issueKey: string) => {
+            if (!issueTestRegex.test(issueKey)) {
+                console.log(`logseq-jira: Badly structured issueKey ${issueKey}`);
             }
-        });
-        
-        const data = await response.json();
-        if (response.status >= 300) {
-            const error = data.errorMessages[0] || 'Unknown Error';
-            //logseq.UI.showMsg(`Error requesting info for ${issueKey}`);
-            return `[Error ${response.status}|${issueKey}|${error}](${jiraURL})`
-        }
-        return `[${issueKey}|${data?.fields.summary}](${jiraURL})`;
-    } catch (e) {
-        console.log('logseq-jira', e.message);
-        return `${issueKey} : Error.`
-    }
+    
+            const baseURL = logseq.settings?.jiraBaseURL;
+            if (!baseURL) {
+                logseq.UI.showMsg('Jira base URL not set. Update in Plugin settings.')
+                throw new Error('Jira base URL not set.');
+            }
+    
+            const creds = Buffer.from(`${logseq.settings?.jiraUsername}:${logseq.settings?.jiraAPIToken}`).toString("base64");
+            const issueRest = `https://${baseURL}/rest/api/3/issue/${issueKey}`;
+            const jiraURL = `https://${baseURL}/browse/${issueKey}`;
+            let req = await fetch(issueRest, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': `Basic ${creds}`
+                }
+            });
+            let body = await req.json();
+            console.log(body);
+            return { issueKey, body, jiraURL }
+            }
+    );
+    return await Promise.all(promises);
+};
+
+//Generate markdown text from results
+function generateTextFromResponse(responses: any[]): Data {
+    let data: Data = {};
+    responses.forEach(response => {
+        data[response.issueKey] = {
+            text: `[${response.body.key}|${response.body.fields.summary}](${response.jiraURL})`,
+            status: response.body.fields.status.name || "None",
+            type: response.body.fields.issuetype.name || "None",
+            priority: response.body.fields.priority.name || "None",
+            creator: response.body.fields.creator.displayName || "None",
+            reporter: response.body.fields.reporter.displayName || "None",
+            assignee: response.body.fields.assignee?.displayName || "None",
+            fixVersion: response.body.fields.fixVersions[0]?.name || "None"
+        };
+    });
+    return data; 
 }
 
+// Helper function run regex replacements asynchronously and as soon as possible.
+async function replaceAsync(str: string, data: Data) {
+    let newString = str;
+    for (const regex of regexList) {
+            newString = newString.replace(regex, (match, ...args: any[]) => {
+                const { issue } = args.pop();
+                return data[issue].text;
+            });
+    }
+    return newString;
+}
 
 const settings: SettingSchemaDesc[] = [
     {
@@ -143,6 +210,41 @@ const settings: SettingSchemaDesc[] = [
         type: "string",
         default: "orgname.atlassian.net",
         title: "Base URL for your organization",
+    },
+    {
+        key: "jiraShowAssignee",
+        description: "Enable this to add Assignee to block properties.",
+        type: "boolean",
+        default: "false",
+        title: "Show Assignee",
+    },
+    {
+        key: "jiraShowPriority",
+        description: "Enable this to add Assignee to block properties.",
+        type: "boolean",
+        default: "false",
+        title: "Show Priority",
+    },
+    {
+        key: "jiraShowFixVersion",
+        description: "Enable this to add *Fix Version* to block properties.",
+        type: "boolean",
+        default: "false",
+        title: "Show Fix Version",
+    },
+    {
+        key: "jiraShowStatus",
+        description: "Enable this to add issue *Status* to block properties.",
+        type: "boolean",
+        default: "false",
+        title: "Show Ticket Status",
+    },
+    {
+        key: "jiraShowReporter",
+        description: "Enable this to add *Reporter* to block properties.",
+        type: "boolean",
+        default: "false",
+        title: "Show Reporter",
     },
 ];
 
