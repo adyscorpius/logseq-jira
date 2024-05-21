09c353a75e7616b3fa3925435ed993a0fa26baa5
Adit Sharda <mailme@aditsharda.com>
2024-05-20 20:34:22 -0400

    chore: Add axios to requests.

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,10 @@ import type { Settings } from './models';
 import { logseq as PL } from "../package.json";
 import { extractIssues, regexList, getAuthHeader } from "./utils";
 
+// Add Axios and support caching.
+import Axios from 'axios';
+
+const axios = Axios.create();
 
 // @ts-expect-error
 const css = (t, ...args) => String.raw(t, ...args);
@@ -104,7 +108,7 @@ async function updateJiraIssue(useSecondOrg: boolean = false): Promise<void> {
 
     const value = currentBlock.content;
     const uuid = currentBlock.uuid;
-    
+
     const issuesList = extractIssues(value);
 
     if (!issuesList || issuesList.length < 1) {
@@ -115,7 +119,7 @@ async function updateJiraIssue(useSecondOrg: boolean = false): Promise<void> {
     const issues = await getIssues(issuesList, useSecondOrg);
 
     const data = generateTextFromResponse(issues);
-    console.log(data);
+
     let newValue = value;
     if (logseq.settings?.updateInlineText) {
       newValue = await replaceAsync(value, data);
@@ -137,33 +141,36 @@ async function getIssues(issuesList: Array<string>, useSecondOrg = false) {
 
   const baseURL = useSecondOrg ? logseq.settings?.jiraBaseURL2 : logseq.settings?.jiraBaseURL;
   const token = useSecondOrg ? logseq.settings?.jiraAPIToken2 : logseq.settings?.jiraAPIToken;
-  const user = useSecondOrg ? logseq.settings?.jiraUsername2 : logseq.settings?.jiraUsername;  
+  const user = useSecondOrg ? logseq.settings?.jiraUsername2 : logseq.settings?.jiraUsername;
   const apiVersion = useSecondOrg ? logseq.settings?.jiraAPIVersion2 : logseq.settings?.jiraAPIVersion || "3";
 
   if (!baseURL || !token || !user) {
-      logseq.UI.showMsg('Jira credentials not set. Update in Plugin settings.')
-      throw new Error('Jira base URL not set.');
+    logseq.UI.showMsg('Jira credentials not set. Update in Plugin settings.')
+    throw new Error('Jira base URL not set.');
   }
 
   const creds: string = btoa(`${user}:${token}`);
   const authHeader = getAuthHeader(useSecondOrg, token, user, creds);
 
-  const requests = issuesList.map(async (issueKey: string) => { 
-      const issueRest = `https://${baseURL}/rest/api/${apiVersion}/issue/${issueKey}`;
-      const jiraURL = `https://${baseURL}/browse/${issueKey}`;
+  const requests = issuesList.map(async (issueKey: string) => {
+    const issueRest = `https://${baseURL}/rest/api/${apiVersion}/issue/${issueKey}`;
+    const jiraURL = `https://${baseURL}/browse/${issueKey}`;
 
-      let response = await fetch(issueRest, {
-          headers: {
-              'Accept': 'application/json',
-              'Authorization': authHeader
-          }
-      });
-      const body = await response.json();
-      return { issueKey, body, jiraURL }
+    const body = await axios.get(issueRest, {
+      id: 'get-issues',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': authHeader
+      }
+    });
+
+    return { issueKey, body, jiraURL }
   }
   );
 
-  return await Promise.all(requests);
+  const response = await Promise.all(requests);
+
+  return response;
 };
 
 // Generate markdown text from response data
@@ -171,20 +178,55 @@ function generateTextFromResponse(responses: any[]): Data {
   const data: Data = {};
 
   responses.forEach((response) => {
-    data[response.issueKey] = {
-      text: `[${response.body.key}|${response.body.fields.summary}](${response.jiraURL})`,
-      summary: response.body.fields.summary ?? 'None',
-      status: response.body.fields.status?.name ?? 'None',
-      type: response.body.fields.issuetype?.name ?? 'None',
-      priority: response.body.fields.priority?.name ?? 'None',
-      creator: response.body.fields.creator?.displayName ?? 'None',
-      reporter: response.body.fields.reporter?.displayName ?? 'None',
-      assignee: response.body.fields.assignee?.displayName ?? 'None',
-      fixVersion: response.body.fields.fixVersions?.[0]?.name ?? 'None',
-      resolution: response.body.fields.resolution?.name ?? null,
+    const {
+      jiraURL,
+      body: {  
+        data: {
+          key,
+          fields: {
+            summary,
+            status,
+            issuetype,
+            priority,
+            creator,
+            reporter,
+            fixVersions,
+            assignee,
+            resolution
+
+            /*
+            created,
+            issuelinks,
+            statuscategory.
+            statuscategorychangedate,
+            project: {
+              key,
+              name,
+
+              projectCategory: {
+                description:
+                name:
+              }
+            }
+
+            */
+
+        }}
+      }
+    } = response;
+    data[key] = {
+      text: `[${key}|${summary}](${jiraURL})`,
+      summary: summary ?? 'None',
+      status: status?.name ?? 'None',
+      type: issuetype?.name ?? 'None',
+      priority: priority?.name ?? 'None',
+      creator: creator?.displayName ?? 'None',
+      reporter: reporter?.displayName ?? 'None',
+      assignee: assignee?.displayName ?? 'None',
+      fixVersion: fixVersions?.[0]?.name ?? 'None',
+      resolution: resolution?.name ?? null,
     };
   });
-
   return data;
 }
 
